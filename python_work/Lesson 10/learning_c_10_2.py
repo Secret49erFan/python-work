f567b40f920a141029ccb47c3205745367f17ae3
Secret49erFan <alexontheiphone@gmail.com>
2025-03-03 08:01:22 +0000

    Update learning_c_10_2.py

---
+++ python_work/Lesson 10/learning_c_10_2.py
@@ -0,0 +1,11 @@
+# practice 3/2/25
+# chapter 10 - learning c
+# learning_c_10_2.py
+from pathlib import Path as p
+path = p('python_work/Lesson 10/learning_python.txt')
+contents = path.read_text()
+# lines = contents.splitlines()
+print(contents)
+for line in contents.splitlines():
+    line.replace('python', 'c')
+    print(line)
