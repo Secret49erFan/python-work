804f520a0f51ea8ebaa789cee1e0119a257d3d3f
Secret49erFan <alexontheiphone@gmail.com>
2025-03-18 05:29:00 +0000

    Practice 3-17-25

--- python_work/lesson_11/city_functions.py
+++ python_work/lesson_11/city_functions.py
@@ -1,6 +1,9 @@
 # practice 3/17/25
 # chapter 11 - city, country
 # city_functions.py
-def city_country(city, country):
-    formatted_city = f'{city}, {country}'
-    return formatted_city.title()
+def city_country(city, country, population=''):
+    if population:
+        formatted_str = f'{city}, {country} -- population {population}'
+    else:
+        formatted_str = f'{city}, {country}'
+    return formatted_str.title()
--- python_work/lesson_11/test_cities.py
+++ python_work/lesson_11/test_cities.py
@@ -3,6 +3,11 @@
 # test_cities.py
 from city_functions import city_country
 def test_city_country():
-    '''Does the city and country return formatted'''
-    formatted_city = city_country('santiago', 'chile')
-    assert formatted_city == 'Santiago, Chile'
+    '''Does the city and country format correctly without a population.'''
+    formatted_str = city_country('santiago', 'chile')
+    assert formatted_str == 'Santiago, Chile'
+
+def test_city_country_population():
+    '''Does the city, country, and population return formatted.'''
+    formatted_str = city_country('santiago', 'chile', '5000000')
+    assert formatted_str == 'Santiago, Chile -- Population 5000000'
