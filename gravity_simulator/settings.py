dc6c0a6eb61ff681c9a5642f8a58066002d9a8fb
Alex Flores <alexontheiphone@gmail.com>
2025-03-24 00:06:28 -0700

    Update the_matrix.py, settings.py

--- gravity_simulator/settings.py
+++ gravity_simulator/settings.py
@@ -6,4 +6,4 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (4,32,4)
+        self.bg_color = (0,5,0) # Deep green color.
--- gravity_simulator/the_matrix.py
+++ gravity_simulator/the_matrix.py
@@ -11,24 +11,29 @@ class TheMatrix:
         '''Initialize the matrix and create The One.'''
         pygame.init()
         self.clock = pygame.time.Clock() # Create a clock to set FPS.
+        self.settings = Settings() # Create a settings class to manage settings.
         self.screen = pygame.display.set_mode((self.settings.screen_width,
-                                               self.settings.screen_height))
+                                               self.settings.screen_height)) # Sets the width & height of the window.
         pygame.display.set_caption('The Matrix') # Just a label.
 
     def run_simulation(self):
-        '''Begin the simualtion.'''
+        '''Begin the simulation.'''
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT: # If the event type is QUIT then exit gracefuly.
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(24)
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        '''Watch for keyboard and mouse events.'''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # If the event type is QUIT then exit gracefuly.
+                sys.exit()
 
-            # Make most recently drawn screen visible.
-            pygame.display.flip() # Essentially refreshes the screen to display the lates drawing updates.
-            self.clock.tick(24) 
+    def _update_screen(self):
+        '''Update images on the screen to the new screen'''
+        self.screen.fill(self.settings.bg_color)
+        # Essentially refreshes the screen to display the lates drawing updates.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a Matrix and start the simulation.
