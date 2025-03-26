04ef26dce4cffae90a4a4e0167d995cc9be53057
Alex Flores <alexontheiphone@gmail.com>
2025-03-25 22:17:30 -0700

    Update the_matrix.py

--- gravity_simulator/the_matrix.py
+++ gravity_simulator/the_matrix.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from circle import Circle
 
 class TheMatrix:
     '''A space where gravity will be simulated.'''
@@ -16,12 +17,16 @@ class TheMatrix:
                                                self.settings.screen_height)) # Sets the width & height of the window.
         pygame.display.set_caption('The Matrix') # Just a label.
 
+        self.circle = Circle(self.settings.screen_width/2, self.settings.screen_height/2, 7, (250,250,250), self) # too much hard coded here. consider moving to settings.py
+
     def run_simulation(self):
         '''Begin the simulation.'''
         while True:
             self._check_events()
             self._update_screen()
             self.clock.tick(24)
+
+            # MOTION GOES HERE
 
     def _check_events(self):
         '''Watch for keyboard and mouse events.'''
@@ -32,6 +37,7 @@ class TheMatrix:
     def _update_screen(self):
         '''Update images on the screen to the new screen'''
         self.screen.fill(self.settings.bg_color)
+        self.circle.blitme()
         # Essentially refreshes the screen to display the lates drawing updates.
         pygame.display.flip()
 
