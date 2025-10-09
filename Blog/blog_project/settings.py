c4459825bbcba3cc2cce3dd910ab721d7814e245
Alex Flores <alexontheiphone@gmail.com>
2025-10-08 22:32:03 -0700

    Excercise 19-2 Blog update.

--- Blog/blog_project/settings.py
+++ Blog/blog_project/settings.py
@@ -35,7 +35,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
     'blogs',
+    'accounts',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'blogs:index'
+LOGOUT_REDIRECT_URL = 'blogs:index'
